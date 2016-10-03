a4b85937100de3f69e52f24902c5ea845b81fa59
jmzavala <jmzavala.olv@gmail.com>
2016-10-02 22:55:34 -0300

    first commit

---
+++ sound/sound/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sound
+//
+//  Created by Juan Manuel Zavala Olave on 02-10-16.
+//  Copyright © 2016 hardleaf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
